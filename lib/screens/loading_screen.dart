33d762dfc04122444010308e772451039ee2a6ef
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-20 11:15:18 +0600

    networking with flutter http package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
 
+import 'package:http/http.dart' as http;
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -11,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -20,8 +23,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitute);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02");
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
